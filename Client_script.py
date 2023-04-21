c5149d9033958038d71882c73432e40a17c17a7d
AurelienLohmann <lohmann.aurelien@gmail.com>
2023-04-21 03:24:41 +0100

    Uploading to public repo

---
+++ Client_script.py
@@ -0,0 +1,54 @@
+##########
+#
+#   Author: Aurélien LOHMANN
+#
+##########
+
+import socket
+import pickle
+
+
+def process_event_data(event_data):
+    # Process the received event data as needed (e.g., display or analyze)
+    print("Received event data:", event_data)
+
+
+def main():
+    # Configuration
+    server_ip = '127.0.0.1'  # Replace with the IP address of the server
+    server_port = 12345      # The same port number as the server
+
+    # Create a socket and connect to the server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    print(f"Connected to server at {server_ip}:{server_port}")
+
+    try:
+        while True:
+            # Receive serialized data from the server
+            serialized_data = b''
+            while True:
+                chunk = client_socket.recv(4096)
+                if not chunk:
+                    break
+                serialized_data += chunk
+
+                # Check if the serialized data is complete
+                try:
+                    event_data = pickle.loads(serialized_data)
+                    break
+                except EOFError:
+                    # If the data is incomplete, keep receiving more data
+                    continue
+
+            # Deserialize and process the event data
+            process_event_data(event_data)
+
+    except KeyboardInterrupt:
+        print("\nClient disconnecting.")
+    finally:
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Server_script.py
@@ -0,0 +1,64 @@
+##########
+#
+#   Author: Aurélien LOHMANN
+#
+##########
+
+import socket
+import pickle
+import time
+
+
+def get_event_data():
+    # Replace this function with the actual code to read data from your event camera.
+    event_data = [
+        {'x': 10, 'y': 20, 'timestamp': time.time(), 'polarity': 1},
+        {'x': 15, 'y': 25, 'timestamp': time.time(), 'polarity': 0},
+    ]
+    return event_data
+
+
+def main():
+    # Configuration
+    host = '0.0.0.0'  # Listen on all available network interfaces
+    port = 12345       # Choose an appropriate port number
+
+    # Create a socket and bind it to the host and port
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)  # Listen for up to 5 pending connections
+
+    print(f"Server listening on {host}:{port}")
+
+    try:
+        while True:
+            # Accept a client connection
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection from {client_address}")
+
+            try:
+                while True:
+                    # Get event data from the event camera
+                    event_data = get_event_data()
+
+                    # Serialize the event data using pickle
+                    serialized_data = pickle.dumps(event_data)
+
+                    # Send the serialized data to the client
+                    client_socket.sendall(serialized_data)
+
+                    # Wait before sending the next set of event data
+                    time.sleep(0.01)
+            except (ConnectionResetError, BrokenPipeError):
+                print(f"Connection closed by {client_address}")
+            finally:
+                client_socket.close()
+
+    except KeyboardInterrupt:
+        print("\nServer shutting down.")
+    finally:
+        server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
